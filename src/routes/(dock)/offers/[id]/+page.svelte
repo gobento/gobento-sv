c0629e5f98836eff55eb176fbb667c521a17466d
fhuber <felix.huber@ametras.com>
2025-11-04 22:54:50 +0000

    image fetching fixes for favorites

--- src/routes/(dock)/offers/[id]/+page.svelte
+++ src/routes/(dock)/offers/[id]/+page.svelte
@@ -6,7 +6,6 @@
 	import IconFluentArrowRight24Filled from '~icons/fluent/arrow-right-24-filled';
 
 	import PaymentModal from './PaymentModal.svelte';
-	import LocationCard from '$lib/components/LocationCard.svelte';
 	import OfferStatusCard from './OfferStatusCard.svelte';
 	import PriceDisplay from '$lib/components/PriceDisplay.svelte';
 	import OptimizedLocationImage from '$lib/components/images/OptimizedLocationImage.svelte';
@@ -63,6 +62,20 @@
 	const handleReserve = () => {
 		showPaymentModal = true;
 	};
+
+	import { browser } from '$app/environment';
+
+	let LocationCard = $state<typeof import('$lib/components/LocationCard.svelte').default | null>(
+		null
+	);
+
+	$effect(() => {
+		if (browser) {
+			import('$lib/components/LocationCard.svelte').then((m) => {
+				LocationCard = m.default;
+			});
+		}
+	});
 </script>
 
 <!-- Location Photo Header (if location exists) -->
@@ -135,16 +148,18 @@
 <!-- Pickup Location -->
 {#if data.location}
 	<div class="px-6">
-		<LocationCard
-			name={data.location.name}
-			address={data.location.address}
-			city={data.location.city}
-			province={data.location.province}
-			zipCode={data.location.zipCode}
-			country={data.location.country}
-			latitude={data.location.latitude}
-			longitude={data.location.longitude}
-		/>
+		{#if LocationCard}
+			<LocationCard
+				name={data.location.name}
+				address={data.location.address}
+				city={data.location.city}
+				province={data.location.province}
+				zipCode={data.location.zipCode}
+				country={data.location.country}
+				latitude={data.location.latitude}
+				longitude={data.location.longitude}
+			/>
+		{/if}
 	</div>
 {:else}
 	<div class="flex items-center gap-3 px-6">
--- src/routes/(dock)/offers/[id]/+page.ts
+++
@@ -1,2 +0,0 @@
-export const ssr = false;
-// If both `ssr` and `csr` are `false`, nothing will be rendered!
