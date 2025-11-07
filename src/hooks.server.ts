0816c47af5835daff1820f2071033a5e6547e6ef
fhuber <felix.huber@ametras.com>
2025-11-07 22:27:40 +0000

    rate limit

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,36 @@
 // src/hooks.server.ts
-import { validateSessionToken } from '$lib/server/auth';
-import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+import { validateSessionToken } from '$lib/server/auth';
+
+import { TokenBucket } from '$lib/server/rate-limit';
+import { sequence } from '@sveltejs/kit/hooks';
+
+import { paraglideMiddleware } from '$lib/paraglide/server';
+import { error, type Handle } from '@sveltejs/kit';
+
+import '@valibot/i18n/de/schema';
+
+const bucket = new TokenBucket<string>(100, 1);
+
+const rateLimitHandle: Handle = async ({ event, resolve }) => {
+	// Note: Assumes X-Forwarded-For will always be defined.
+	const clientIP = event.request.headers.get('X-Forwarded-For');
+	if (clientIP === null) {
+		return resolve(event);
+	}
+	let cost: number;
+	if (event.request.method === 'GET' || event.request.method === 'OPTIONS') {
+		cost = 1;
+	} else {
+		cost = 3;
+	}
+	if (!bucket.consume(clientIP, cost)) {
+		throw error(429, 'Too many requests');
+	}
+	return resolve(event);
+};
+
+const authHandle: Handle = async ({ event, resolve }) => {
 	console.log('🔍 Session - Checking session');
 	const sessionToken = event.cookies.get('session');
 
@@ -26,3 +54,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 
 	return resolve(event);
 };
+
+const paraglideHandle: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+		event.request = localizedRequest;
+		return resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%lang%', locale)
+		});
+	});
+
+export const handle = sequence(rateLimitHandle, authHandle, paraglideHandle);
